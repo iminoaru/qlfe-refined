48d3fc9e27d55f7d358d94b11c601c77804130e0
sarthak <sarthakgaud04@gmail.com>
2024-06-27 15:48:16 +0530

    load dump

--- components/EnterpriseFeatures.tsx
+++ components/EnterpriseFeatures.tsx
@@ -4,21 +4,21 @@
 //     return (
 //         <section className="section-enterprise-features">
 //             <div className="enterprise-features_grid">
-//                 <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a10a-93cc38bc"
+//                 <div   
 //                      className="enterprise-features_item-wrapper">
-//                     <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a10b-93cc38bc"
+//                     <div    
 //                          className="enterprise-features_item">
-//                         <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a10c-93cc38bc"
+//                         <div     
 //                              className="enterprise-features_top">
 //                             <div className="subtitle">New Questions added everyday</div>
 //                             <Image
 //                                 src="https://cdn.prod.website-files.com/663fa3b3696c8bf893cc38b7/66719ad7a034bf9a0b522c0a_ezgif.com-optimize%20(2).gif"
 //                                 width={300} height={200} alt="New questions" className="enterprise-gif"/>
 //                         </div>
-//                         <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a134-93cc38bc" className="grid_stack">
-//                             <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a135-93cc38bc"
+//                         <div id="    grid-s1" className="grid_stack">
+//                             <div id="    grid-s2"
 //                                  className="enterprise-features_number">
-//                                 <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a136-93cc38bc"
+//                                 <div  
 //                                      className="enterprise-features_title">100+<span
 //                                     className="text-unit">Questions</span>
 //                                 </div>
@@ -26,23 +26,23 @@
 //                         </div>
 //                     </div>
 //                 </div>
-//                 <div id="w-node-fdc5a485-973b-2f21-515f-bdf92e3eb214-93cc38bc"
+//                 <div id="hero-s1"
 //                      className="background-color-border"></div>
-//                 <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a178-93cc38bc"
+//                 <div  
 //                      className="enterprise-features_item-wrapper">
-//                     <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a179-93cc38bc"
+//                     <div   
 //                          className="enterprise-features_item">
-//                         <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a17a-93cc38bc"
+//                         <div    
 //                              className="enterprise-features_top">
 //                             <div className="subtitle">These questions will reduce your calculation timing and improve
 //                                 your logical thinking.
 //                             </div>
 //
 //                         </div>
-//                         <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a2-93cc38bc" className="grid_stack">
-//                             <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a3-93cc38bc"
+//                         <div id="   grid-s3" className="grid_stack">
+//                             <div id="   grid-s4"
 //                                  className="enterprise-features_number">
-//                                 <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a4-93cc38bc"
+//                                 <div  
 //                                      className="enterprise-features_title">2.54<span
 //                                     className="text-unit">x Faster</span>
 //                                 </div>
@@ -51,18 +51,18 @@
 //                     </div>
 //                 </div>
 //
-//                 <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a178-93cc38bc"
+//                 <div  
 //                      className="enterprise-features_item-wrapper">
-//                     <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a179-93cc38bc"
+//                     <div   
 //                          className="enterprise-features_item">
-//                         <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a17a-93cc38bc"
+//                         <div    
 //                              className="enterprise-features_top">
 //                             <div className="subtitle">Your new box content goes here.</div>
 //                         </div>
-//                         <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a2-93cc38bc" className="grid_stack">
-//                             <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a3-93cc38bc"
+//                         <div id="   grid-s3" className="grid_stack">
+//                             <div id="   grid-s4"
 //                                  className="enterprise-features_number">
-//                                 <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a4-93cc38bc"
+//                                 <div  
 //                                      className="enterprise-features_title">Your new box title<span
 //                                     className="text-unit">Your new box unit</span>
 //                                 </div>
@@ -83,65 +83,64 @@ export default function EnterpriseFeatures() {
     return (
         <section className="section-enterprise-features">
             <div className="enterprise-features_grid">
-                <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a10a-93cc38bc"
+                <div   
                      className="enterprise-features_item-wrapper">
-                    <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a10b-93cc38bc"
+                    <div    
                          className="enterprise-features_item">
-                        <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a10c-93cc38bc"
+                        <div     
                              className="enterprise-features_top">
                             <div className="subtitle">New Questions added everyday</div>
                             <Image
                                 src="https://cdn.prod.website-files.com/663fa3b3696c8bf893cc38b7/66719ad7a034bf9a0b522c0a_ezgif.com-optimize%20(2).gif"
                                 width={300} height={200} alt="New questions" className="enterprise-gif"/>
                         </div>
-                        <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a134-93cc38bc" className="grid_stack">
-                            <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a135-93cc38bc"
+                        <div id="grid-s1" className="grid_stack">
+                            <div id="grid-s2"
                                  className="enterprise-features_number">
-                                <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a136-93cc38bc"
+                                <div  
                                      className="enterprise-features_title">100+<span className="text-unit">Questions</span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div id="w-node-fdc5a485-973b-2f21-515f-bdf92e3eb214-93cc38bc"
+                <div id="hero-s1"
                      className="background-color-border"></div>
-                <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a178-93cc38bc"
-                     className="enterprise-features_item-wrapper">
-                    <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a179-93cc38bc"
+                <div className="enterprise-features_item-wrapper">
+                    <div   
                          className="enterprise-features_item">
-                        <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a17a-93cc38bc"
+                        <div    
                              className="enterprise-features_top">
                             <div className="subtitle">Start for free by solving our starter questions
                             </div>
                         </div>
-                        <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a2-93cc38bc" className="grid_stack">
-                            <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a3-93cc38bc"
+                        <div id="grid-s1" className="grid_stack">
+                            <div id="grid-s2"
                                  className="enterprise-features_number">
-                                <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a4-93cc38bc"
+                                <div  
                                      className="enterprise-features_title">Improvement <span className="text-unit">starts now</span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div id="w-node-fdc5a485-973b-2f21-515f-bdf92e3eb244-93cc38bc"
+                <div  
                      className="background-color-border"></div>
 
-                <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a178-93cc38bc"
+                <div  
                      className="enterprise-features_item-wrapper">
-                    <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a179-93cc38bc"
+                    <div   
                          className="enterprise-features_item">
-                        <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a17a-93cc38bc"
+                        <div    
                              className="enterprise-features_top">
                             <div className="subtitle">These questions will reduce your calculation timing and improve
                                 your logical thinking
                             </div>
                         </div>
-                        <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a2-93cc38bc" className="grid_stack">
-                            <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a3-93cc38bc"
+                        <div id="grid-s1" className="grid_stack">
+                            <div id="grid-s2"
                                  className="enterprise-features_number">
-                                <div id="w-node-_2fa532b1-c0f6-9bf9-6c07-fc0da0b6a1a4-93cc38bc"
+                                <div  
                                      className="enterprise-features_title">1.56<span className="text-unit">x Faster</span>
                                 </div>
                             </div>
--- components/SubpageCTA.tsx
+++ components/SubpageCTA.tsx
@@ -9,13 +9,13 @@ export default function SubpageCTA() {
                     <div className="padding-section-small">
                         <div className="grid_stack text-align-center text-color-black">
                             <div className="spacer-medium"></div>
-                            <div id="w-node-c85ee8de-d6cd-cd2c-72c3-3c570fbf2c50-93cc38bc" className="position-relative">
-                                <h1 id="w-node-c85ee8de-d6cd-cd2c-72c3-3c570fbf2c51-93cc38bc" className="heading_subpage-hero">Start your Journey of Mental Maths</h1>
+                            <div id=" cta-s1" className="position-relative">
+                                <h1 className="heading_subpage-hero">Start your Journey of Mental Maths</h1>
                             </div>
-                            <div id="w-node-c85ee8de-d6cd-cd2c-72c3-3c570fbf2c53-93cc38bc" className="spacer-1rem"></div>
-                            <div id="w-node-c85ee8de-d6cd-cd2c-72c3-3c570fbf2c54-93cc38bc" className="text_supage-hero">Take the first step in transforming your brain to a Mental Math Gym. You wont believe how affordable we are!!</div>
-                            <div id="w-node-c85ee8de-d6cd-cd2c-72c3-3c570fbf2c56-93cc38bc" className="spacer-medium"></div>
-                            <Link href="https://subs-prototype-stripe.vercel.app/" id="w-node-c85ee8de-d6cd-cd2c-72c3-3c570fbf2c57-93cc38bc" className="button-black wide w-inline-block">
+                            <div  className="spacer-1rem"></div>
+                            <div  className="text_supage-hero">Take the first step in transforming your brain to a Mental Math Gym. You wont believe how affordable we are!!</div>
+                            <div  className="spacer-medium"></div>
+                            <Link href="https://subs-prototype-stripe.vercel.app/" id="cta-s2" className="button-black wide w-inline-block">
                                 <div>View Pricing</div>
                             </Link>
                         </div>
